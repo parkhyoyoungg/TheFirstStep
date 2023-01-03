a59ac9d330733f0a460aac09abe2d5d187154472
parkhyoyoungg <121841633+parkhyoyoungg@users.noreply.github.com>
2023-01-03 14:03:47 +0900

    Add files via upload

---
+++ main/js/script.js
@@ -0,0 +1,40 @@
+ $(function(){
+ var swiper = new Swiper('.one', {
+  autoplay:false,
+	  // autoplay:
+    // {
+    //     delay:3500,//슬라이드 넘어가는 시간
+    //     disableOnInteraction: false,
+    //   },
+      // loop: true,
+      //슬라이드 무한반복
+
+      navigation: {//화살표 버튼
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	    pagination: {//블릿 버튼
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+});
+
+
+ $(function(){
+ var swiper = new Swiper('.two', {
+	  autoplay: {
+        delay: 3500,//슬라이드 넘어가는 시간
+        disableOnInteraction: false,
+      },
+      loop: true,//슬라이드 무한반복
+      navigation: {//화살표 버튼
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	    pagination: {//블릿 버튼
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+});
